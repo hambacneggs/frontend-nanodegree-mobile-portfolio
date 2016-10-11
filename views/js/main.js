34d7074d8109d8b97f01dcf2cee40b39101c1bc6
Jon Osgood <jon360@gmail.com>
2016-10-11 14:27:14 -0700

    add comment about updatePositions changes

--- views/js/main.js
+++ views/js/main.js
@@ -489,6 +489,7 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  // moved this out of the 'for loop', it only needs to be calculated once
   var scrollNum = document.body.scrollTop / 1250;
 
   for (var i = 0; i < items.length; i++) {
